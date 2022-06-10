df5eb287387bd941d88c5bb153b2ca987de266c7
atul <atulmishra.bhumca09@gmail.com>
2022-06-10 13:19:52 +0530

    Create index.js

---
+++ pages/dealers/[dealerId]/index.js
@@ -0,0 +1,11 @@
+function DealerSitesPage() {
+    return (
+        <div>
+            <h1>
+                List of sites!!
+            </h1>
+        </div>
+    );
+}
+
+export default DealerSitesPage;
